8400e61d96f13478fcf00afd68e5c80640e7687b
OnneniKumara <156514572+OnneniKumara@users.noreply.github.com>
2025-02-21 19:38:21 +0200

    feat: Add total cost calculation to loan application; update template to display estimated total cost
    <-- commit viestit jälleen GH copilotilta, muu omaa

--- src/app/components/tasks/course-exam/models/exam-loan-application.ts
+++ src/app/components/tasks/course-exam/models/exam-loan-application.ts
@@ -3,6 +3,7 @@ export class ExamLoanApplication {
   loanAmount: number;
   loanDuration: number;
   estimatedMonthlyCost: number;
+  totalCost: number;
   sosSecNumber: string;
   firstName: string;
   lastName: string;
@@ -13,6 +14,7 @@ export class ExamLoanApplication {
     this.loanAmount = 500;
     this.loanDuration = 1;
     this.estimatedMonthlyCost = 0;
+    this.totalCost = 0;
     this.sosSecNumber = "";
     this.firstName = "";
     this.lastName = "";
@@ -27,6 +29,13 @@ export class ExamLoanApplication {
     this.estimatedMonthlyCost = ((Math.pow((1 + intrestRate / 1200), 12 * this.loanDuration) * intrestRate / 1200) /
       ((Math.pow((1 + intrestRate / 1200), 12 * this.loanDuration)) - 1) * this.loanAmount);
     return this.estimatedMonthlyCost;
+  }
+
+  // laskee lainan kokonaiskustannuksen
+
+  calculateTotalCost() {
+    this.totalCost = this.estimatedMonthlyCost * 12 * this.loanDuration;
+    return this.totalCost;
   }
 
 }
