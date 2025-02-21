ddc8f256632f692d717c257c538da8baae6c5d9e
OnneniKumara <156514572+OnneniKumara@users.noreply.github.com>
2025-02-21 18:14:08 +0200

    feat: Add loan application form to LoanCalculator component; implement input validation and submission handling
    <-- commit messaget GH-copilotilta, koodit omaa. (käytännössä copy-pastesin osan sisällöstä template driven form harjoituksesta)

--- src/app/components/tasks/course-exam/loan-calculator/loan-calculator.component.ts
+++ src/app/components/tasks/course-exam/loan-calculator/loan-calculator.component.ts
@@ -1,4 +1,4 @@
-import {Component, Optional} from '@angular/core';
+import {Component, inject, Optional} from '@angular/core';
 import {CdkDrag, CdkDragHandle} from "@angular/cdk/drag-drop";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
@@ -9,6 +9,10 @@ import {Router} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {MatSlider, MatSliderThumb} from '@angular/material/slider';
 import {ExamLoanApplication} from '../models/exam-loan-application';
+import {ExamLoanApplicationService} from '../services/exam-loan-application.service';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-loan-calculator',
@@ -25,7 +29,12 @@ import {ExamLoanApplication} from '../models/exam-loan-application';
     NgIf,
     FormsModule,
     MatSlider,
-    MatSliderThumb
+    MatSliderThumb,
+    MatCheckbox,
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel
   ],
   templateUrl: './loan-calculator.component.html',
   styleUrl: './loan-calculator.component.css'
@@ -39,11 +48,14 @@ export class LoanCalculatorComponent {
               @Optional() private dialogRef: MatDialogRef<LoanCalculatorComponent>) {
   }
 
+  // lainahakemuksen hyväksyminen
+  isTickChecked: boolean = false;
+
+  // injektoidaan lainahakemuspalvelu
+  protected loanService = inject(ExamLoanApplicationService);
 
   // lainahakemus
   loanApplication: ExamLoanApplication = new ExamLoanApplication();
-
-
 
 
   // funktio joka tarkistaa onko dialogi aktiivinen
@@ -66,4 +78,10 @@ export class LoanCalculatorComponent {
       this.active = false;
     }
   }
+
+  // mennään 'etusivulle'
+  cancel() {
+    this.router.navigate(['']);
+  }
+
 }
