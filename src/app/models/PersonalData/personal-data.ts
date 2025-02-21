deff19c3834d2c4c05846ef69757ecc83f713008
OnneniKumara <156514572+OnneniKumara@users.noreply.github.com>
2025-02-21 12:21:34 +0200

    feat: Add PersonalData class and corresponding unit tests; update template form validation styles

---
+++ src/app/models/PersonalData/personal-data.ts
@@ -0,0 +1,18 @@
+export class PersonalData {
+
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+
+  constructor() {
+    this.username = "";
+    this.firstname = "";
+    this.lastname = "";
+    this.email = "";
+    this.password = "";
+    this.confirmPassword = "";
+  }
+}
