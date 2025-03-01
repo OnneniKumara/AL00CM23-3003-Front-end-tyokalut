b92de071696b735ea2bf03da6f86cc074982dd3a
OnneniKumara <156514572+OnneniKumara@users.noreply.github.com>
2025-03-01 23:38:47 +0200

    feat: Add Finnkino filter pipe and search functionality to news component

--- src/app/components/tasks/finnkino-news/finnkino-news.component.ts
+++ src/app/components/tasks/finnkino-news/finnkino-news.component.ts
@@ -1,9 +1,12 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject,} from '@angular/core';
 import {FinnkinoApiGetterService} from '../../../services/finnkino-api-getter.service';
 import {PopUpService} from '../../../services/pop-up.service';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {AsyncPipe, DatePipe, NgClass, NgForOf,} from '@angular/common';
-
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {FinnKinoFilterPipe} from '../../../pipes/finn-kino-filter.pipe';
 
 @Component({
   selector: 'app-finnkino-news',
@@ -12,13 +15,19 @@ import {AsyncPipe, DatePipe, NgClass, NgForOf,} from '@angular/common';
     AsyncPipe,
     NgForOf,
     DatePipe,
-    NgClass
+    NgClass,
+    MatFormField,
+    MatInput,
+    FormsModule,
+    FinnKinoFilterPipe,
   ],
   templateUrl: './finnkino-news.component.html',
   styleUrl: './finnkino-news.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FinnkinoNewsComponent {
+
+  protected searchString: string = "";
 
   panelOpenState: boolean[] = [];
   // otetaan käyttöön FinnkinoApiGetterService
@@ -32,6 +41,7 @@ export class FinnkinoNewsComponent {
   private popUpService = inject(PopUpService);
 
   // Initialize the panelOpenState array based on the number of items
+
   ngOnInit() {
     this.finnKinoData$.subscribe(data => {
       this.panelOpenState = new Array(data.length).fill(false);
